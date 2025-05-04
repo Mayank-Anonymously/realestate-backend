496e7a00564bd329b1892717600d7b77c906729e
Mayank-Anonymously <craftaar0@gmail.com>
2025-05-04 22:43:35 +0530

    addded changes

--- src/routes/propertyRouter.js
+++ src/routes/propertyRouter.js
@@ -1,8 +1,12 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const propertyController = require('../controller/propertyController');
+const propertyController = require("../controller/propertyController");
 
-router.get('/get-detail-by-id/:title/:rent', propertyController.getAllProperties);
-router.post('/properties', propertyController.createProperty);
+router.get(
+  "/get-detail-by-id/:title/:rent",
+  propertyController.getAllProperties
+);
+router.post("/properties", propertyController.createProperty);
+router.get("/search-properties/:query", propertyController.searchProperty);
 
 module.exports = router;
