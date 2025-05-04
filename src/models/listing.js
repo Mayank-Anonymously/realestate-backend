7c398f7a49d08f035aa44b1ad29bf5d8e2fdbe71
Mayank-Anonymously <craftaar0@gmail.com>
2025-05-04 11:24:37 +0530

    added changes

--- src/models/listing.js
+++ src/models/listing.js
@@ -1,13 +1,12 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const listingSchema = new mongoose.Schema({
-  id: { type: String, required: true, unique: true },
   title: String,
   address: String,
   description: String,
   contact: String,
   price: Number,
-  image: String
+  image: String,
 });
 
-module.exports = mongoose.model('Listing', listingSchema);
+module.exports = mongoose.model("Listing", listingSchema);
