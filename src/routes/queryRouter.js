2c3560de3a38a9c534aaa7377aa4aa45698388c3
Mayank Kumar <developer-at-work@Mayanks-MacBook-Air.local>
2025-06-10 08:50:12 +0530

    added

--- src/routes/queryRouter.js
+++ src/routes/queryRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const queryrouter = express.Router();
 const { submitQuery, getAllQuery } = require('../controller/queryController');
 
-// POST /contact/query
 queryrouter.post('/query', submitQuery);
 queryrouter.get('/get-all-queries/CRM', getAllQuery);
 
